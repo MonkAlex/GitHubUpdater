8480c4665f4a3287cae1a153dcfa9a6cd6d44a28
MonkAlex <3amepob@gmail.com>
2016-08-21 07:41:53 +0400

    Черновой набросок.

---
+++ GitHubUpdater.CLI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GitHubUpdater.CLI
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(string.Join("', '", args));
+      if (args.Length < 2)
+      {
+        Console.WriteLine("Run with repoId and current tagName. RepoId from https://api.github.com/repos/{owner}/{repo}");
+        return;
+      }
+
+      AsyncMain(int.Parse(args[0]), args[1]).Wait();
+    }
+
+    static async Task<bool> AsyncMain(int repositoryId, string tagName)
+    {
+      var canUpdate = await Shared.DownloadUpdate.HasUpdate(repositoryId, tagName);
+      if (canUpdate)
+      {
+        var files = await Shared.DownloadUpdate.Download(repositoryId);
+        foreach (var file in files)
+        {
+          Console.WriteLine(string.Format("Downloaded: {0}", file));
+        }
+      }
+      return true;
+    }
+  }
+}
---
+++ GitHubUpdater.Shared/DownloadUpdate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace GitHubUpdater.Shared
+{
+  public static class DownloadUpdate
+  {
+    private const string UserAgent = "MonkAlex-MangaReader";
+    private static readonly string WorkFolder = AppDomain.CurrentDomain.BaseDirectory;
+
+    public async static Task<bool> HasUpdate(int repositoryId, string tagName)
+    {
+      var release = await GetLatestRelease(repositoryId);
+      return !string.Equals(tagName, release.TagName, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public async static Task<List<string>> Download(int repositoryId)
+    {
+      var release = await GetLatestRelease(repositoryId);
+      return Download(release);
+    }
+
+    private static List<string> Download(Release release)
+    {
+      var result = new List<string>();
+      Parallel.ForEach(release.Assets, (asset) =>
+      {
+        var dl = new WebClient();
+        var data = dl.DownloadData(asset.BrowserDownloadUrl);
+        var path = Path.Combine(WorkFolder, asset.Name);
+        File.WriteAllBytes(path, data);
+        result.Add(path);
+      });
+      return result;
+    }
+
+    private static Task<Release> GetLatestRelease(int repositoryId)
+    {
+      var client = new GitHubClient(new ProductHeaderValue(UserAgent));
+      return client.Repository.Release.GetLatest(repositoryId);
+    }
+  }
+}
