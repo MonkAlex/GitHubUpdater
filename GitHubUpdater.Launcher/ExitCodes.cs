5c51ee82fb9f9d72953879fb02f8aca7e0fc840d
MonkAlex <3amepob@gmail.com>
2016-10-01 16:13:35 +0400

    Наброски лаунчера, надо придумать, как собирать конкретную версию.

---
+++ GitHubUpdater.Launcher/ExitCodes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubUpdater.Launcher
+{
+  [Flags]
+  public enum ExitCodes
+  {
+    None = 0,
+    WebsiteOpened = 1,
+    AnyVersionNotFound = 2,
+    AnyExeFileNotFound = 4
+  }
+}
---
+++ GitHubUpdater.Launcher/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubUpdater.Launcher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var directory = AppDomain.CurrentDomain.BaseDirectory;
+      var versions = Directory.GetDirectories(directory);
+      if (!versions.Any())
+      {
+        OpenWebsite(ExitCodes.AnyVersionNotFound);
+        return;
+      }
+
+      var lastVersion = versions
+        .Select(v => new {path = v, version = new Version(Path.GetFileName(v))})
+        .OrderBy(c => c.version)
+        .First()
+        .path;
+
+      InitVersion(lastVersion, args);
+    }
+
+    private static void OpenWebsite(ExitCodes code)
+    {
+      Process.Start(@"https://github.com/MonkAlex/GitHubUpdater/releases/latest");
+      var iCode = ExitCodes.WebsiteOpened | code;
+      Close(iCode);
+    }
+
+    private static void Close(ExitCodes code)
+    {
+      Environment.Exit((int)code);
+    }
+
+    private static void InitVersion(string lastVersion, string[] args)
+    {
+      var canBeStarted = Directory.GetFiles(lastVersion, "*.exe");
+      if (!canBeStarted.Any())
+      {
+        OpenWebsite(ExitCodes.AnyExeFileNotFound);
+        return;
+      }
+
+      if (canBeStarted.Length == 1)
+      {
+        Process.Start(canBeStarted[0], string.Join(" ", args));
+        Close(ExitCodes.None);
+      }
+
+      // TODO : Докрутить какую то фигню, чтобы сама запускала консольный или WPF варианты.
+    }
+  }
+}
