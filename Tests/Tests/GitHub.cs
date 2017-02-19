7ca70fa30b28b8b537a7f670165cacdb6b001760
MonkAlex <3amepob@gmail.com>
2017-02-19 08:57:51 +0400

    Simple test to github api\limits.

---
+++ Tests/Tests/GitHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using GitHubUpdater.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+  [TestClass]
+  public class GitHub
+  {
+    [TestMethod]
+    public void GetReleases()
+    {
+      var option = new Option() { RepositoryId = 66179868 };
+      var du = new DownloadUpdate(option);
+      var hu = du.HasUpdate();
+      Assert.IsTrue(hu.Result);
+      var files = du.GetFiles();
+      var hasFiles = files.Result.Any();
+      Assert.IsTrue(hasFiles);
+    }
+  }
+}
